2925135cac76fae77486af9c52677f671c7de52f
Theriley106 <christopherlambert106@gmail.com>
2017-10-14 01:38:25 -0400

    Added the ability to grab profile information from devpost

--- api.py
+++ api.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response
-import login
 import requests
 import re
+import bs4
 
 
 app = Flask(__name__)
 
 
 
-@app.route('/login&<username>&<password>')
-def fileDownloading(username, password):
+@app.route('/login&<username>&<password>', methods=['GET', 'POST'])
+def loginPage(username, password):
 	a = manage(username, password)
-	return str(a.status)
+	return str(a.grabProfile())
 
 
 
@@ -38,6 +38,28 @@ class manage(object):
 		else:
 			status = True
 		return {"Status": status}
+
+	def grabProfile(self, url='https://hackgt2017.devpost.com/participants?page=', name='.participant-name a'):
+		information = []
+		r = self.session.get(url)
+		page = bs4.BeautifulSoup(r.text, "lxml")
+		header = page.select("h3")
+		participants = int(re.findall('\d+', str(header[0].getText()))[0])
+		print participants
+		if participants % 20 != 0:
+			num = ((20 - (participants % 20)) + participants) / 20
+		else:
+			num = participants / 20
+		for e in range(1, num):
+			r = requests.get(url + str(e))
+			page = bs4.BeautifulSoup(r.text, "lxml")
+			for user in page.select(name):
+				information.append(user)
+		return information
+
+
+
+		
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8888)
