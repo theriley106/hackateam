1015164c0f6dc4188261fc6897f919be9c0c8c9c
SushmitDharurkar <dharurkarsushmit@gmail.com>
2017-10-14 19:42:28 -0400

    refactoring

--- devpost_website_scrapper.py
+++ devpost_website_scrapper.py
@@ -16,50 +16,23 @@ def grabHackathons(events):
     i = 0
     for pages in range(1, num+1):
         i = request_stuff(events, pages, i)
-        #print (events)
-        #print ("---------------")
 
 def request_stuff(events, page, i):
     devpost_url = "https://devpost.com/hackathons?page=" + str(page)
     devpost_html = requests.get(devpost_url)
     soup = BeautifulSoup(devpost_html.content, "lxml")
     event_titles = soup.select(".title")
-    #event_locations = soup.select(".challenge-location")
     event_descriptions = soup.select(".challenge-description")
-    '''event_prizes_dates = soup.select(".value")
-    event_prizes = [None] * (len(event_prizes_dates)/2)
-    event_dates = [None] * (len(event_prizes_dates)/2)'''
-    #event_prizes = soup.select(".prizes .value")
-    #print (len(event_prizes))
     event_dates = soup.select(".date-range")
 
-    #If prize is non-monetory
-    #if len(event_prizes) < len(event_titles):
-#        event_prizes = soup.select(".prizes")
-#    print (event_prizes)
     #If date abnormal
     if len(event_dates) < len(event_titles):
         event_dates = soup.select(".submission-time .value")
 
-    '''j = 0
-    k = 0
-    for index in range(0, len(event_prizes_dates)):
-        if index % 2 == 0:
-            event_prizes[k] = event_prizes_dates[index].getText().strip()
-            k+=1
-        else:
-            event_dates[j] = event_prizes_dates[index].getText().strip()
-            j+=1'''
-
-    #i = 0
     for index in range(0, len(event_titles)):
         event = {}
         event["name"] = event_titles[index].getText().strip()
-        #event["location"] = event_locations[index].getText().strip()
         event["description"] = event_descriptions[index].getText().strip()
-        #event["prizes"] = event_prizes[index]
-        #event["dates"] = event_dates[index]
-#        event["prizes"] = event_prizes[index].getText().strip()
         event["dates"] = event_dates[index].getText().strip()
         events[i] = event
         i += 1
@@ -70,7 +43,6 @@ def request_stuff(events, page, i):
 def index():
     events = {}
     grabHackathons(events)
-    #request_stuff(events)
     return jsonify(events)
 
 if __name__ == '__main__':
